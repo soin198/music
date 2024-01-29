4f7e98c7f4b3521f26af6cf6bbcf4799a9387216
“soin198@foxmail” <soin198@foxmail.com>
2024-01-29 16:17:16 +0800

    初始化模块

---
+++ soin-website/src/main/java/org/soin/website/controller/dto/SubmitCommentDto.java
@@ -0,0 +1,36 @@
+package org.soin.website.controller.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.soin.core.biz.common.BaseParams;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * 提交评论
+ *
+ * @author J.FLa.Soin
+ * @version 1.0.0
+ * @date 2024-01-08 15:53
+ **/
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SubmitCommentDto extends BaseParams implements Serializable {
+
+    private static final long serialVersionUID = 7881075963677007809L;
+
+    /**
+     * 音乐ID
+     */
+    @NotNull(message = "请提供音乐ID")
+    private Long musicId;
+
+    /**
+     * 评论内容
+     */
+    @NotBlank(message = "请提供评论内容")
+    private String content;
+
+}
