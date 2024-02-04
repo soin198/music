fc7c1dbdaafd94c4a39000874b90cca23a8babc8
“soin198@foxmail” <soin198@foxmail.com>
2024-02-04 10:46:02 +0800

    swagger处理

--- soin-client/src/main/java/org/soin/client/api/ICustomApi.java
+++ soin-client/src/main/java/org/soin/client/api/ICustomApi.java
@@ -1,7 +1,5 @@
 package org.soin.client.api;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.soin.client.api.dto.RegisterDTO;
 import org.soin.core.infrastructure.base.GenericResponse;
 import org.soin.core.infrastructure.base.constant.BaseConstant;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @version 1.0.0
  * @date 2024-01-31 14:37
  **/
-@Api(value = "ICustomApi", tags = "用户中心")
 @RequestMapping(BaseConstant.BASE_SERVICE_URL + "/custom/authority")
 public interface ICustomApi {
 
@@ -25,7 +22,7 @@ public interface ICustomApi {
      * @param username 用户名
      * @param password 密码
      * @return token
-     */@ApiOperation(value = "登录")
+     */
     @PostMapping("/login")
     GenericResponse<String> login(@RequestParam(value = "username") String username,
                                   @RequestParam(value = "password") String password);
@@ -37,7 +34,6 @@ public interface ICustomApi {
      * @param registerDTO 注册数据源
      * @return 是否注册成功
      */
-    @ApiOperation(value = "注册")
     @PostMapping("/register")
     GenericResponse<Boolean> register(@RequestBody RegisterDTO registerDTO);
 
