29dae41d95cab899caa3d581d8741afc0cb9cdb0
J.Fla.soin <soin198@foxmail.com>
2024-02-27 11:33:58 +0800

    集成XXL

--- music-server/soin-core/src/main/java/org/soin/core/infrastructure/utils/RegexExpressionUtil.java
+++ music-server/soin-core/src/main/java/org/soin/core/infrastructure/utils/RegexExpressionUtil.java
@@ -1,6 +1,5 @@
 package org.soin.core.infrastructure.utils;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -16,8 +15,9 @@ public class RegexExpressionUtil {
      * 校验是否是手机号
      */
     public static boolean isMobile(String mobile) {
-        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(16[5,6])|(17[0-8])|(18[0-9])|(19[1、5、8、9]))\\d{8}$";
-        return Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(mobile).matches();
+        int length = mobile.length();
+        String regex = "1\\d{10}";
+        return Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(mobile).matches() && (length == 11);
     }
 
     /**
